644e1b6464a914f6e7b0fe7e67c7aa27f7923580
Lucas <lucas.briere@efrei.net>
2022-02-17 17:25:29 +0100

    feat: insta links

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,9 +1,30 @@
-nav = document.querySelector('header nav');
+nav = document.querySelector("header nav");
 
-nav.addEventListener('click', () => {
-	if (nav.classList.contains('open')) {
-		nav.classList.remove('open');
-	} else {
-		nav.classList.add('open');
-	}
+nav.addEventListener("click", () => {
+  if (nav.classList.contains("open")) {
+    nav.classList.remove("open");
+  } else {
+    nav.classList.add("open");
+  }
+});
+
+const lightbox = document.createElement("div");
+lightbox.id = "lightbox";
+document.body.appendChild(lightbox);
+
+const images = document.querySelectorAll("img");
+images.forEach((image) => {
+  image.addEventListener("click", (e) => {
+    lightbox.classList.add("active");
+    const img = document.createElement("img");
+    img.src = image.src;
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild);
+    }
+    lightbox.appendChild(img);
+  });
+});
+
+lightbox.addEventListener("click", (e) => {
+  lightbox.classList.remove("active");
 });
