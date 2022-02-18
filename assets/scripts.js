714c21c62c2af880ad581091ea23c4a8f13bae44
Lucas <lucas.briere@efrei.net>
2022-02-18 01:22:12 +0100

    feat: reveal functionality

--- assets/scripts.js
+++ assets/scripts.js
@@ -28,3 +28,27 @@ images.forEach((image) => {
 lightbox.addEventListener("click", (e) => {
   lightbox.classList.remove("active");
 });
+
+/*
+ * REVEAL METHOD
+ */
+const ratio = 0.1;
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: ratio,
+};
+
+const handleIntersect = function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > ratio) {
+      entry.target.classList.add("reveal-visible");
+      observer.unobserve(entry.target);
+    }
+  });
+};
+
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll(".reveal").forEach(function (r) {
+  observer.observe(r);
+});
